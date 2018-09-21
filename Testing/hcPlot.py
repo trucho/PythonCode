53cc7f11ed061624deb42972829847c36d3d65e2
trucho <angueyra@gmail.com>
2018-09-21 17:55:48 -0400

    Made plots for every cell and found a way to tile them

--- Testing/hcPlot.py
+++ Testing/hcPlot.py
@@ -6,27 +6,37 @@ def rotate(x,y,angle,ox,oy):
     x_rot = ox + math.cos(angle) * (x - ox) - math.sin(angle) * (y - oy)
     y_rot = oy + math.sin(angle) * (x - ox) + math.cos(angle) * (y - oy)
     return x_rot,y_rot
-    
+
+def xy_angle(x1,y1,x2,y2):
+    theta = (math.atan2(y2,x2) - math.atan2(y1,x1)) * (180.0 / math.pi)
+    theta % 360
+    return theta
+
+def xy_distance(x1,y1,x2,y2):
+    dist=math.sqrt(math.pow((x2-x1),2) + math.pow((y2-y1),2))
+    return dist
+
 class hcCanvas:
-    filename = 'test-svgwrite'
-    x_size = 800
-    y_size = 800
+    x_size = 650
+    y_size = 500
     axColor = "rgb(200,200,200)"
     axColorMin = "rgb(230,230,230)"
+    axGonColor = "rgb(255,255,255)"
     axSW=2
     axSWGon=1
     axMax=25
     scalar = axMax*1/3; #to transform from pixels to units
     z_gap = (axMax+15) * scalar
     h_pentagon = (axMax * scalar * (math.sqrt(5+2*math.sqrt(5))/16))
-    def __init__(self):
-        self.x_center = self.x_size/2
+    def __init__(self,svgfilename):
+        self.filename = svgfilename
+        self.x_center = self.x_size/2-60
         self.y_center = self.y_size/2
         self.x_origin = self.x_center
         self.y_origin = self.y_size-self.y_center
         self.colors = options = {'uv':'rgb(127,15,126)','s':'rgb(11,36,251)','m':'rgb(15,127,18)','l':'rgb(252,13,27)','r':'rgb(0,0,0)','z':'rgb(253,189,64)'}
         self.canvas = svgwrite.Drawing(filename = self.filename + ".svg",size = (self.x_size, self.y_size))
-        self.canvas.add(self.canvas.rect(insert = (0,0),size = (self.x_size, self.y_size),id='frame',stroke="black",fill="rgb(240,240,240)"))
+        # self.canvas.add(self.canvas.rect(insert = (0,0),size = (self.x_size, self.y_size),id='frame',stroke="black",fill="rgb(240,240,240)"))
 #         self.addOrigin();
         self.addAxis();
         
@@ -85,7 +95,7 @@ class hcCanvas:
                 rotate(self.x_origin,self.y_origin-self.scalar*scale,360*3/5,self.x_origin,self.y_origin),
                 rotate(self.x_origin,self.y_origin-self.scalar*scale,360*4/5,self.x_origin,self.y_origin),
             ],
-                id='axGon',fill="white",stroke = self.axColor,stroke_width=self.axSWGon)
+                id='axGon',fill=self.axGonColor,stroke = self.axColor,stroke_width=self.axSWGon)
         )
         
     def drawAxgonMinor(self,scale):
@@ -105,7 +115,7 @@ class hcCanvas:
             self.canvas.rect(
                 insert = (self.x_center+self.z_gap+2.5-20,self.y_center+((self.axMax-scale) * self.scalar)-self.h_pentagon),
                 size = (40,scale * self.scalar),
-                id='zaxGon',fill="white",stroke = self.axColor,stroke_width=self.axSWGon)
+                id='zaxGon',fill=self.axGonColor,stroke = self.axColor,stroke_width=self.axSWGon)
         )
     
     def drawZAxGonMinor(self,scale):
@@ -141,7 +151,7 @@ class hcCanvas:
 class plotDatum:
     #Class attributes
     w = 5;
-    def __init__(self,dwg,x=0,y=0,h=0,stroke="black",fill="none",rotate=0):
+    def __init__(self,dwg,x=0,y=0,h=0,stroke="none",fill="black",rotate=0):
         self.dwg = dwg
         self.canvas = dwg.canvas
         self.h = h * self.dwg.scalar
@@ -205,55 +215,60 @@ class plotDatum:
 
 class uBar(plotDatum):
     def __init__(self,dwg,h):
-        super().__init__(dwg,dwg.x_center,dwg.y_center,h,dwg.colors.get('uv'),"none",360*0/5)
-        # self.addDot('uDat')
+        super().__init__(dwg,dwg.x_center,dwg.y_center,h,"none",dwg.colors.get('uv'),360*0/5)
 
 class sBar(plotDatum):
     def __init__(self,dwg,h):
-        super().__init__(dwg,dwg.x_center,dwg.y_center,h,dwg.colors.get('s'),"none",360*1/5)
-        # self.addDot('sDat')
+        super().__init__(dwg,dwg.x_center,dwg.y_center,h,"none",dwg.colors.get('s'),360*1/5)
+        
 
 class mBar(plotDatum):
     def __init__(self,dwg,h):
-        super().__init__(dwg,dwg.x_center,dwg.y_center,h,dwg.colors.get('m'),"none",360*2/5)
-        # self.addDot('mDat')
+        super().__init__(dwg,dwg.x_center,dwg.y_center,h,"none",dwg.colors.get('m'),360*2/5)
 
 class lBar(plotDatum):
     def __init__(self,dwg,h):
-        super().__init__(dwg,dwg.x_center,dwg.y_center,h,dwg.colors.get('l'),"none",360*3/5)
-        # self.addDot('lDat')
+        super().__init__(dwg,dwg.x_center,dwg.y_center,h,"none",dwg.colors.get('l'),360*3/5)
 
 class rBar(plotDatum):
     def __init__(self,dwg,h):
-        super().__init__(dwg,dwg.x_center,dwg.y_center,h,dwg.colors.get('r'),"none",360*4/5)
-        # self.addDot('rDat')
+        super().__init__(dwg,dwg.x_center,dwg.y_center,h,"none",dwg.colors.get('r'),360*4/5)
 
 class zBar(plotDatum):
     def __init__(self,dwg,h):
-        super().__init__(dwg,dwg.x_center+dwg.z_gap,dwg.y_center,h,dwg.colors.get('z'),"none",0)
-        self.addzDot('zDat')
+        super().__init__(dwg,dwg.x_center+dwg.z_gap,dwg.y_center,h,"none",dwg.colors.get('z'),0)
         
 class plotCell:
     def __init__(self,dwg,cellData):
         self.dwg = dwg
         self.cellData = cellData
+        #create structure and gather data
         self.uDat = uBar(self.dwg,self.cellData[0]);
         self.sDat = sBar(self.dwg,self.cellData[1]);
         self.mDat = mBar(self.dwg,self.cellData[2]);
         self.lDat = lBar(self.dwg,self.cellData[3]);
         self.rDat = rBar(self.dwg,self.cellData[4]);
         self.zDat = zBar(self.dwg,self.cellData[5]);
+        #plot Polygon
         self.polyCell();
+        #plot individual dots 
+        self.uDat.addDot('uDat');
+        self.sDat.addDot('sDat');
+        self.mDat.addDot('mDat');
+        self.lDat.addDot('lDat');
+        self.rDat.addDot('rDat');
+        self.zDat.addzDot('zDat');
         
     def polyCell(self):
+        uStr="M" + str(int(self.uDat.x_real)) + "," + str(int(self.uDat.y_real))
+        sStr="L" + str(int(self.sDat.x_real)) + "," + str(int(self.sDat.y_real))
+        mStr="L" + str(int(self.mDat.x_real)) + "," + str(int(self.mDat.y_real))
+        lStr="L" + str(int(self.lDat.x_real)) + "," + str(int(self.lDat.y_real))
+        rStr="L" + str(int(self.rDat.x_real)) + "," + str(int(self.rDat.y_real))
+        d_command=" ".join([uStr,sStr,mStr,lStr,rStr, "Z"])
         self.poly = self.dwg.canvas.add(
-            svgwrite.shapes.Polygon(
-                points=[
-                    [self.uDat.x_real,self.uDat.y_real],
-                    [self.sDat.x_real,self.sDat.y_real],
-                    [self.mDat.x_real,self.mDat.y_real],
-                    [self.lDat.x_real,self.lDat.y_real],
-                    [self.rDat.x_real,self.rDat.y_real]
-                ],
-                id='cellGon',fill="none",stroke = "rgb(0,0,0)",stroke_width=3,stroke_opacity="0.5")
+            svgwrite.path.Path(d=d_command,
+            id='cellGon',
+            stroke_linecap="round",
+            fill="none",stroke = "rgb(0,0,0)",stroke_width=3,stroke_opacity="1")
         )
